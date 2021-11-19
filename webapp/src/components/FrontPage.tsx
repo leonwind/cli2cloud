e9e74159899e19b9ec9db13dc2b4b3f6a5bfed9c
Leon Windheuser <leon.windheuser@datadoghq.com>
2021-11-19 16:09:02 +0100

    Style search bar

--- webapp/src/components/FrontPage.tsx
+++ webapp/src/components/FrontPage.tsx
@@ -2,23 +2,24 @@ import React, {ChangeEvent, Component, FormEvent} from "react";
 import styles from "../styles/FrontPage.module.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form } from "react-bootstrap";
-import SearchIcon from "@material-ui/icons/Search"
+import { Navigate } from "react-router-dom";
 
 interface State {
-    clientID: string
+    clientID: string,
+    redirect: boolean
 }
 
 export class FrontPage extends Component<{}, State> {
-
+    
     constructor(props: any) {
         super(props);
         this.state = {
-            clientID: ""
+            clientID: "",
+            redirect: false
         };
 
         this.handleClientIDChange = this.handleClientIDChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-
     }
 
     private handleClientIDChange(event: ChangeEvent<HTMLInputElement>) {
@@ -28,28 +29,38 @@ export class FrontPage extends Component<{}, State> {
     private handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
         console.log(this.state.clientID);
+        this.setState({redirect: true})
     } 
 
     render() {
+        if (this.state.redirect) {
+            return (
+                <Navigate to={"/" + this.state.clientID}/>
+            )
+        }
         return (
             <div className={styles.body}>
+
                 <h1 className={styles.headline}>
                     Cli2Cloud
                 </h1>
-                
-                <div className={styles.searchForm}>
+
+                <br/>
+                <br/>
+
                 <Form onSubmit={this.handleSubmit}>
-                    <Form.Group className="mb-3">
-                        <SearchIcon fontSize="large" className={styles.searchIcon}/>
-                        <Form.Control value={this.state.clientID} onChange={this.handleClientIDChange} size="lg" type="text" 
-                            placeholder="Enter your terminal ID" />
-                        <br/>
-                        <h6 className={styles.mediumText}>
-                            Monitor your terminal output from everywhere.
-                        </h6>
-                    </Form.Group>
-                </Form>
+                <div className={styles.searchForm}> 
+                        <Form.Control className={styles.searchForm} value={this.state.clientID} onChange={this.handleClientIDChange} size="lg" type="text" 
+                            placeholder="Enter your terminal ID..."/>
                 </div>
+                </Form>
+
+                <br/>
+                <br/>
+
+                <h6 className={styles.mediumText}>
+                    Monitor and Share Your Yerminal Output with Everyone from Everywhere in Realtime.
+                </h6>
             </div>
         )
     }
