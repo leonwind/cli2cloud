4719221750e7323fe3c9a7ca320e96d4cb210843
Leon Windheuser <leon.windheuser@datadoghq.com>
2021-11-19 14:00:08 +0100

    Style frontpage
    Fix css

--- webapp/src/components/FrontPage.tsx
+++ webapp/src/components/FrontPage.tsx
@@ -1,4 +1,8 @@
-import React, {Component} from 'react';
+import React, {ChangeEvent, Component, FormEvent} from "react";
+import styles from "../styles/FrontPage.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from "react-bootstrap";
+import SearchIcon from "@material-ui/icons/Search"
 
 interface State {
     clientID: string
@@ -11,12 +15,41 @@ export class FrontPage extends Component<{}, State> {
         this.state = {
             clientID: ""
         };
+
+        this.handleClientIDChange = this.handleClientIDChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
+
+    private handleClientIDChange(event: ChangeEvent<HTMLInputElement>) {
+        this.setState({clientID: event.target.value});
+    }
+
+    private handleSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(this.state.clientID);
+    } 
 
     render() {
         return (
-            <div>
-                Cli2Cloud
+            <div className={styles.body}>
+                <h1 className={styles.headline}>
+                    Cli2Cloud
+                </h1>
+                
+                <div className={styles.searchForm}>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group className="mb-3">
+                        <SearchIcon fontSize="large" className={styles.searchIcon}/>
+                        <Form.Control value={this.state.clientID} onChange={this.handleClientIDChange} size="lg" type="text" 
+                            placeholder="Enter your terminal ID" />
+                        <br/>
+                        <h6 className={styles.mediumText}>
+                            Monitor your terminal output from everywhere.
+                        </h6>
+                    </Form.Group>
+                </Form>
+                </div>
             </div>
         )
     }
