7013579647ccc13bdbefe9d424ea584a804a1a4f
Leon Windheuser <leon.windheuser@datadoghq.com>
2022-03-04 01:33:51 +0100

    Fix

--- webapp/src/components/Decrypt.ts
+++ webapp/src/components/Decrypt.ts
@@ -16,8 +16,8 @@ export class DecryptionService {
     }
 
     public constructor(password: string, salt: string, iv: string) {
-        salt = "dfdf45b433ba4a3082ffd106a688b4c1b3e5efa07e73e208c46178e9abb9d816";
-        iv = "e787923d9d0b874eecb45a6677a66f8c";
+        salt = "836440f899b9050ffeed785004bd095d63bc6c6d22264f6da562d6d142199e36";
+        iv = "bf8ecd752ca283b2b40d8a0bf30b6176";
         this.key = this.kdf(password, cryptoJS.enc.Hex.parse(salt))
         console.log("Keys:");
         console.log(this.key.toString());
@@ -29,30 +29,22 @@ export class DecryptionService {
             iv: cryptoJS.enc.Hex.parse(iv),
             padding: cryptoJS.pad.Pkcs7,
         });
-
-        const plaintext = "PING google.com (216.58.213.78): 56 data bytes";
-        const encrypted = cryptoJS.enc.Hex.parse("703575fe8fd11e356b02892173a724e511955f9636119244440b93c8b52bf701997e9318dc0a923671d67dbc1c5492");
+        
+        const encrypted = cryptoJS.enc.Hex.parse("bb1f27dae3afa314e0c8a3e8ce06183399f208618ada31c7979e8a047e2cce11f29ab9e0d4df6aef25efefd1124a0ad5");
         let dec = decryptor.finalize(encrypted).toString(cryptoJS.enc.Latin1);
         console.log(dec)
         
-        //let suffix = decryptor.finalize();
-        //stdout.write(suffix.toString(cryptoJS.enc.Latin1));
-        const encrypted2 = cryptoJS.enc.Hex.parse("686a6de4e0374644df773fb18ade30dbd2c5240fbedfa20c8ca30ebd90e1813f4e19d9e58d24dfe36c19c683b8b550077729267f3fbdef97ce462b65874b");
+        const encrypted2 = cryptoJS.enc.Hex.parse("540810c10e1842500282c84749f71e287cdf9bf19699844f0329523771362fc4b6a1ad91644acfee28b252937ecd915cf4684743aa133f25b1a6c4f71a1ce74e");
         let dec2 = decryptor.finalize(encrypted2).toString(cryptoJS.enc.Latin1);
         console.log(dec2);
         
-        /*
-        const encrypted3 = cryptoJS.enc.Hex.parse("f27e361a971a541bc81756bf9ed66303e05531d86e92ef22202029512ea16e7ce8fd2f021c05b9502b41535ad2d757af4a457a5dde80ff08033e8e250ac1");
-        let dec3 = decryptor.process(encrypted3);
+        const encrypted3 = cryptoJS.enc.Hex.parse("fe5ff7383fb3190055aa9138c78d592775b1c39652122b1a16cca169bef168948869784bd3c9b9fb4f1799efa80bc6a19f782f62cf4f627ae184a45bde298731");
+        let dec3 = decryptor.finalize(encrypted3);
         console.log(dec3.toString(cryptoJS.enc.Latin1));
 
-        const encrypted4 = cryptoJS.enc.Hex.parse("b6d93b561dd289727d893b2a5c6c6e3c13506e2cb1289edd542b073cb18371b8af2bc05ff118749e347483d361305d317035dbb2788212f5a4847ba823fe");
+        const encrypted4 = cryptoJS.enc.Hex.parse("e8d5e110a662e141a0493cf5d1dd21762e8b69269beeb1ef1cfb6597614bb33b8284d11693fb90a3227cabe0fc232e76a6fd2f7f26692c14f86ac169769fdf34");
         let dec4 = decryptor.finalize(encrypted4);
         console.log(dec4.toString(cryptoJS.enc.Latin1));
-        */
-        let suffix = decryptor.finalize().toString(cryptoJS.enc.Latin1);
-        console.log(suffix);
-        console.log("###")
     }
 
     public decrypt(encrypted: string): string {
